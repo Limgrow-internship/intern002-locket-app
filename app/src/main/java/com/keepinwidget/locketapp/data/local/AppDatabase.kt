3bb69cea589570df1c858cf9912b4b4aff8941e3
Nguyễn Long Vũ <nguyenlongvu22122003@gmail.com>
2025-11-05 15:05:18 +0700

    Refactor: update gitignore and ktlint

--- app/src/main/java/com/keepinwidget/locketapp/data/local/AppDatabase.kt
+++
@@ -1,3 +0,0 @@
-package com.keepinwidget.locketapp.data.local
-
-
--- app/src/main/java/com/keepinwidget/locketapp/di/AppModule.kt
+++ app/src/main/java/com/keepinwidget/locketapp/di/AppModule.kt
@@ -15,7 +15,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
     private const val BASE_URL = "https://your-api-url.com/"
 
     @Provides
